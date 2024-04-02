0259a7c92b430555d5c1adc8242900e10522a1d6
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-02 13:15:16 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,7 +2,10 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const timer = document.querySelector(".timer")
-
+setInterval(() => {
+    const data = new Date
+    console.log(data)
+}, interval);
 
 
 
