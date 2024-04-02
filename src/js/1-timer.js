762c8e472e6033295ee7577f9f9612983571b0e6
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-02 20:34:36 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,46 +10,55 @@ flatpickr("#datetime-picker", {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose: function (selectedDates) {
-        const currentDate = new Date();
-        const selectedDate = selectedDates[0]
-        if (selectedDate >= currentDate) {
-             btn.removeAttribute('disabled');
-             countdownDate = selectedDate;
-            return
-        } else {
-            window.alert("Please choose a date in the future");
+  onClose: function (selectedDates) {
+    const currentDate = new Date();
+    const selectedDate = selectedDates[0]
+    if (selectedDate >= currentDate) {
+      btn.removeAttribute('disabled');
+      countdownDate = selectedDate;
+    } else {
+      window.alert("Please choose a date in the future");
     }
-    
   }
 });
-btn.addEventListener("click", btnStartFoo)
-function btnStartFoo() {
-    const now = new Date();
-    const ms = countdownDate.getTime() - now.getTime();
-    btn.setAttribute('disabled', "");
 
-    function convertMs(ms) {
-  // Number of milliseconds per unit of time
+btn.addEventListener("click", btnStartFoo);
+
+function btnStartFoo() {
+  btn.setAttribute('disabled', "");
+  setInterval(() => {
+    const now = new Date();
+    convertMs(countdownDate.getTime() - now.getTime());
+  }, 1000);
+  convertMs(countdownDate.getTime() - new Date().getTime()); // обновляем таймер сразу после нажатия кнопки
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+// не объявляем переменные заново, а просто обновляем их значения
+function convertMs(ms) {
+  if (ms < 0) {
+    clearInterval();
+    return;
+  }
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
+  const timerDays = document.querySelector("[data-days]");
+  const timerHours = document.querySelector("[data-hours]");
+  const timerMinutes = document.querySelector("[data-minutes]");
+  const timerSeconds = document.querySelector("[data-seconds]");
+  timerDays.textContent = days;
+  timerHours.textContent = hours;
+  timerMinutes.textContent = minutes;
+  timerSeconds.textContent = seconds;
 }
-
-console.log(convertMs(ms));
-
-
-}
-
