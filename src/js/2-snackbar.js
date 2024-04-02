fe9369cc1e6cb14142b1e8870ae3b451a8a9c8d7
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-02 21:31:55 +0200

    f

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,6 +1,38 @@
-const formInput = document.querySelector(".form label input[name='delay']");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
+const form = document.querySelector(".form");
+const formInput = form.querySelector("input[name='delay']");
+const createBtn = form.querySelector("button");
+let fulfilledRadioButton = form.querySelector('input[name="state"][value="fulfilled"]');
+let rejectedRadioButton = form.querySelector('input[name="state"][value="rejected"]');
 
-    console.log(formInput.value);
+form.addEventListener("submit", createPromise);
 
+function createPromise(e) {
+    e.preventDefault(); 
+    const delay = formInput.value;
 
+    const promise = new Promise((resolve, reject) => {
+        if (fulfilledRadioButton.checked) {
+            setTimeout(() => resolve(), delay);
+        } else {
+            setTimeout(() => reject(), delay);
+        }
+    });
+
+    promise
+        .then(() =>
+            iziToast.show({
+                position: "topRight",
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                backgroundColor: "green"
+            })
+        ).catch(() =>
+            iziToast.show({
+                position: "topRight",
+                message: `❌ Rejected promise in ${delay}ms`,
+                backgroundColor: "red"
+            })
+        );
+}
